c9cc29f2ed220df27223f94f4806c80cb2a702e3
Jeferson <jeffher789@gmail.com>
2024-07-11 23:04:22 -0500

    all views functioning, but not set up

--- Hoteles/destinos_colombia/urls.py
+++ Hoteles/destinos_colombia/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("products", views.products, name="products"),
+    path("custServ", views.cust_serv, name="customer"),
+    path("events", views.events, name="events"),
+    path("precios", views.precios, name="precios"),
+    path("restaurants", views.restaurants, name="restaurants"),
+
 ]
--- Hoteles/destinos_colombia/views.py
+++ Hoteles/destinos_colombia/views.py
@@ -6,3 +6,19 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
+def products(request):
+    return render(request, "products.html")
+
+def events(request):
+    return render(request, "events.html")
+
+def precios(request):
+    return render(request, "precios.html")
+
+def restaurants(request):
+    return render(request, "restaurants.html")
+
+def cust_serv(request):
+    return render(request, "custServ.html")
+
+
